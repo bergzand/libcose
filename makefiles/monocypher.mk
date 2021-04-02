abacfeafd3a38425de8becfbbdab4c8a8f494e30
Koen Zandberg <koen@bergzand.net>
2021-04-02 14:54:33 +0200

    Monocypher: Update integration to new API

--- makefiles/monocypher.mk
+++ makefiles/monocypher.mk
@@ -8,11 +8,12 @@ MONOCYPHER_DIR ?= ../Monocypher/
 
 ifeq ($(MONOCYPHER_LOCAL), 1)
   MONOCYPHER_INCLUDE ?= -I$(MONOCYPHER_DIR)/src -I$(MONOCYPHER_DIR)/src/optional
-  MONOCYPHER_LIB ?= $(MONOCYPHER_DIR)/lib/libmonocypher.so
+  MONOCYPHER_OBJ ?= $(MONOCYPHER_DIR)/lib/monocypher.o $(MONOCYPHER_DIR)/lib/monocypher-ed25519.o
+  LDFLAGS += $(MONOCYPHER_OBJ)
 else
   MONOCYPHER_INCLUDE ?= $(shell pkg-config --cflags $(MONOCYPHERLIB))
   MONOCYPHER_LIB ?= $(shell pkg-config --libs $(MONOCYPHERLIB))
+  LDFLAGS_CRYPTO += -Wl,$(MONOCYPHER_LIB)
 endif
 
 CFLAGS_CRYPTO += $(MONOCYPHER_INCLUDE)
-LDFLAGS_CRYPTO += -Wl,$(MONOCYPHER_LIB)
